428166341a701f9e72aea76336d6fb4f4b3885af
Stephen Clibbery <sdclibbery@googlemail.com>
2017-01-16 21:19:34 +0000

    Initial commit

---
+++ ESP8266AirQualityMonitor.ino
@@ -0,0 +1,12 @@
+#define ANALOGUE_IN_PIN A0
+#define LED_OUT_PIN D0
+
+void setup() {
+  pinMode(ANALOGUE_IN_PIN, INPUT);
+  pinMode(LED_OUT_PIN, OUTPUT);
+}
+
+void loop() {
+//  digitalWrite(LED_BUILTIN, HIGH);
+//  delay(1000);
+}
