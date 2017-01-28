440c47a457a15aec0f9a5bcf2b74116204636a37
Stephen Clibbery <sdclibbery@googlemail.com>
2017-01-28 09:13:00 +0000

    Switch to POST to make it work with StatHat

--- ESP8266AirQualityMonitor.ino
+++ ESP8266AirQualityMonitor.ino
@@ -1,18 +1,16 @@
 #include "RunningMedian.h"
 #include <ESP8266WiFi.h>
 
-const int dustPin= A0;
+const int dustPin = A0;
 const float ADC_REF = 3.3;
-const int ADC_MAX = 1024; 
-const int ledPower =D1;
+const int ADC_MAX = 1024;
+const int ledPower = D1;
 
 // WiFi connection
 #include "wifi-credentials.h"
 
 // Data stream
-const char* host = "kitwallace.co.uk";
-const char* streamId   = "stream";
-const char* privateKey = "pk";
+const char* statName = "dust";
 
 // Sharp timing
 int waitToRead = 280;
@@ -23,7 +21,7 @@ int waitForNext= 9680;
 int sampleSize = 15;
 RunningMedian samples = RunningMedian(sampleSize);
 
-int interval = 3000; 
+int interval = 3000;
 float lastReading ;
 void setup(){
    Serial.begin(9600);
@@ -31,24 +29,25 @@ void setup(){
    pinMode(ledPower,OUTPUT);
    lastReading = millis();
 }
- 
+
 void loop(){
-   int baseDigital = analogRead(dustPin);
-   digitalWrite(ledPower,LOW); // power on the LED
-   delayMicroseconds(waitToRead);
-   int readDigital =analogRead(dustPin); 
-   delayMicroseconds(waitToTurnOff);
-   digitalWrite(ledPower,HIGH); // turn the LED off
-   int dustDigital = readDigital - baseDigital;
-   samples.add(dustDigital);
-   if (millis() > lastReading + interval) {
-        float dustVoltage = samples.getAverage() * ADC_REF / ADC_MAX;
-        String report = String("dustVoltage=")+dustVoltage;
-        Serial.println(report);
-        logData(report);
-        lastReading = millis();
-   }
-   delayMicroseconds(waitForNext);
+  int baseDigital = analogRead(dustPin);
+  digitalWrite(ledPower, LOW); // power on the LED
+  delayMicroseconds(waitToRead);
+  int readDigital = analogRead(dustPin);
+  delayMicroseconds(waitToTurnOff);
+  digitalWrite(ledPower, HIGH); // turn the LED off
+  int dustDigital = readDigital - baseDigital;
+  samples.add(dustDigital);
+  if (millis() > lastReading + interval) {
+    Serial.println("");
+    float dustVoltage = samples.getAverage() * ADC_REF / ADC_MAX;
+    String report = String("dustVoltage=")+dustVoltage;
+    Serial.println(report);
+    logData(dustVoltage);
+    lastReading = millis();
+  }
+  delayMicroseconds(waitForNext);
 }
 
 void connectWifi() {
@@ -57,53 +56,50 @@ void connectWifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
- 
+
   WiFi.begin(ssid, password);
- 
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void httpGet(String url) {
-   WiFiClient client;
-   url.replace(" ", "+");
-   int httpPort = 80;
-   while (true) {
+void logData(float value) {
+   String data = "stat=";
+   data += statName;
+   data += "&email=12.hhc.air.quality.monitor@gmail.com&value=";
+   data += value;
+   httpPost("api.stathat.com", "/ez", data);
+}
+
+void httpPost(const char* host, String url, String data) {
+  int httpPort = 80;
+  WiFiClient client;
+  url.replace(" ", "+");
+  Serial.print("POST to: ");
+  Serial.print(host);
+  Serial.println(url);
+  while (true) {
     if (client.connect(host, httpPort)) break;
     Serial.println("connection failed");
     delay(100);
   }
-  
   Serial.println("connected ");
-  Serial.print("requesting URL: ");
-  Serial.println(url);
-// This will send the request to the server
-  String httprequest = String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n";
-// Serial.println(httprequest);
-  client.print(httprequest);
-  delay(100);
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("closing connection");
-}
 
-void logData(String params) {
-   String url = "/rt/home.xq?_action=store&_id=";
-   url += streamId;
-   url += String("&_pk=")+ privateKey + "&";
-   url += params;
-   httpGet(url); 
+  String httprequest = String("POST ") + url + " HTTP/1.1\r\n" +
+              "Host: " + host + "\r\n" +
+              "User-Agent: ESP8266\r\n" +
+              "Accept: */*\r\n" +
+              "Content-Length: " + data.length() + "\r\n" +
+              "Content-Type: application/x-www-form-urlencoded\r\n\r\n" +
+              data;
+//Serial.println(httprequest);
+  client.print(httprequest);
+
+  Serial.println("closing connection");
 }
